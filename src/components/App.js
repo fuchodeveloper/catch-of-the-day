1e4b8e3ca7c3f32c20eb9a960e20f91d01f590de
fucho <fmgbeoma@yahoo.com>
2017-10-11 12:44:13 +0100

    chore: setup states in app

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,23 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.addFish = this.addFish.bind(this);
+    this.state = {
+      fishes: {},
+      order: {}
+    }
+  }
+
+  addFish(fish){
+    const fishes = {...this.state.fishes};
+    // add new fish
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    this.setState({ fishes: fishes });
+  }
+
   render(){
     return (
       <div className="catch-of-the-day">
@@ -11,7 +28,7 @@ class App extends React.Component {
           <Header tagline="Fresh seafood market"/>
         </div>
         <Order/>
-        <Inventory/>
+        <Inventory addFish={this.addFish}/>
       </div>
     )
   }
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,9 +2,15 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  constructor() {
+    super();
+    this.goToStore = this.goToStore.bind(this);
+  }
+
   goToStore(event){
     event.preventDefault();
-    console.log('URL changed.');
+    const storeId = this.storeInput.value;
+    this.context.router.transitionTo(`/store/${storeId}`);
   }
 
   render() {
@@ -12,11 +18,15 @@ class StorePicker extends React.Component {
       <form className="store-selector" onSubmit={this.goToStore}>
         {/* Form to capture user data */}
         <h2>Please enter a store name</h2>
-        <input type="text" name="" id="" defaultValue={getFunName()} required/>
+        <input type="text" defaultValue={getFunName()} ref={(input) => { this.storeInput = input }} required/>
         <button type="submit">Visit store</button>
       </form>
     )
   }
+}
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
 }
 
 export default StorePicker;
